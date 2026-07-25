54d1e42727dd34d64c8e6c4d4bdf24c900e57fd7
Jon Craton <jncraton@gmail.com>
2026-07-25 15:11:48 -0400

    Test random roundtrip

--- camel.js
+++ camel.js
@@ -46,5 +46,7 @@ function decode(text) {
   return text
 }
 
+const rand = 'b*4PzNvM%[&P&O-4c!mmEHQS3$QWokhhQ013P.7}|!CMNs1;ugD=#GBI^,cOWCoV'
+console.assert(decode(encode(rand)) == rand)
 console.assert(decode(encode(sample)) == sample)
 console.log(sample.length, encode(sample).length, encode(sample))
