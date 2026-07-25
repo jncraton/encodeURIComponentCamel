25e8734d2ecce1e549a93f52e6ce353159ba33a3
Jon Craton <jncraton@gmail.com>
2026-07-25 09:29:55 -0400

    Test round trip

--- camel.js
+++ camel.js
@@ -1,3 +1,5 @@
+const sample = "HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects such as interactive forms may be embedded into the rendered page. HTML provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes, and other items."
+
 function swapCamel(text) {
   // Swaps space separated words to camel case and vice versa
   text = text.replace(/([,a-z])( [a-z]|[A-Z])(?=[a-z])/g, (match, before, char) => {
@@ -18,6 +20,7 @@ console.assert(swapCamel('camel case') == 'camelCase')
 console.assert(swapCamel('camel, case') == 'camel,Case')
 console.assert(swapCamel('camel,Case') == 'camel, case')
 console.assert(swapCamel('camel CASE') == 'camel CASE')
+console.assert(swapCamel(swapCamel(sample)) == sample)
 
 function encode(text) {
   text = swapCamel(text)
@@ -37,3 +40,5 @@ function decode(text) {
 
   return text
 }
+
+console.assert(decode(encode(sample)) == sample)
