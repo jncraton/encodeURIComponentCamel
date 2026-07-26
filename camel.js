fd5063dae3ae55f4cfd96f57339e95a59b430456
Jon Craton <jncraton@gmail.com>
2026-07-26 06:12:59 -0400

    Move test code

--- camel.js
+++ camel.js
@@ -1,6 +1,3 @@
-const sample =
-  'HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects such as interactive forms may be embedded into the rendered page. HTML provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes, and other items.\n\nDeCSS is one of the first free computer programs capable of decrypting commercially produced DVD-Video discs. Before the release of DeCSS, free and open source operating systems (such as BSD and Linux) could not play encrypted video DVDs.'
-
 function swapCamel(text) {
   // Swaps space separated words to camel case and vice versa
   text = text.replace(/[,\.a-zA-Z] ?[a-zA-Z](?=[a-z])/g, match => {
@@ -27,19 +24,6 @@ function swapCamel(text) {
   return text
 }
 
-const assert = (a, b) => console.assert(a == b, `${a} != ${b}`)
-
-assert(swapCamel('camelCase'), 'camel case')
-assert(swapCamel('camel case'), 'camelCase')
-assert(swapCamel('camel, case'), 'camel,case')
-assert(swapCamel('camel,case'), 'camel, case')
-assert(swapCamel('Hello. World.'), 'Hello.World.')
-assert(swapCamel('Hello.World.'), 'Hello. World.')
-assert(swapCamel('camel CASE'), 'camel CASE')
-assert(swapCamel('McCoy'), 'Mc coy')
-assert(swapCamel('Mc coy'), 'McCoy')
-assert(swapCamel(swapCamel(sample)), sample)
-
 function encode(text) {
   text = swapCamel(text)
 
@@ -58,6 +42,22 @@ function decode(text) {
 
   return text
 }
+
+const sample =
+  'HTML elements are the building blocks of HTML pages. With HTML constructs, images and other objects such as interactive forms may be embedded into the rendered page. HTML provides a means to create structured documents by denoting structural semantics for text such as headings, paragraphs, lists, links, quotes, and other items.\n\nDeCSS is one of the first free computer programs capable of decrypting commercially produced DVD-Video discs. Before the release of DeCSS, free and open source operating systems (such as BSD and Linux) could not play encrypted video DVDs.'
+
+const assert = (a, b) => console.assert(a == b, `${a} != ${b}`)
+
+assert(swapCamel('camelCase'), 'camel case')
+assert(swapCamel('camel case'), 'camelCase')
+assert(swapCamel('camel, case'), 'camel,case')
+assert(swapCamel('camel,case'), 'camel, case')
+assert(swapCamel('Hello. World.'), 'Hello.World.')
+assert(swapCamel('Hello.World.'), 'Hello. World.')
+assert(swapCamel('camel CASE'), 'camel CASE')
+assert(swapCamel('McCoy'), 'Mc coy')
+assert(swapCamel('Mc coy'), 'McCoy')
+assert(swapCamel(swapCamel(sample)), sample)
 
 const rand = 'b*4PzNvM%[&P&O-4c!m mEHQS3$QWokhhQ013P.7}|!CMN s1;ugD=#GBI^,cOWCoV'
 assert(decode(encode(rand)), rand)
