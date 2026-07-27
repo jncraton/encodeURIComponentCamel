d379baab41480acbfbb664017406f2256b1eaa17
Jon Craton <jncraton@gmail.com>
2026-07-27 16:21:39 -0400

    Avoid space after all caps words

--- camel.js
+++ camel.js
@@ -1,10 +1,10 @@
 function swapCamel(text) {
   // Swaps space separated words to camel case and vice versa
   text = text.replace(/[,\)a-zA-Z] ?[a-zA-Z](?=[a-z])/g, match => {
-    if (match.match(/[\,\)a-z] [a-z]/)) {
+    if (match.match(/[\,\)a-zA-Z] [a-z]/)) {
       // Spaced to camel
       return match[0] + match[2].toUpperCase()
-    } else if (match.match(/[\,\)a-z][A-Z]/)) {
+    } else if (match.match(/[\,\)a-zA-Z][A-Z]/)) {
       // Camel to spaced
       return match[0] + ' ' + match[1].toLowerCase()
     } else {
