9201064360e9a3b78554a68b44abb81ab6903780
Jon Craton <jncraton@gmail.com>
2026-07-27 15:21:44 -0400

    Add deflate comparison

--- test.js
+++ test.js
@@ -17,9 +17,44 @@ assert(swapCamel(swapCamel(sample)), sample)
 const rand = 'b*4PzNvM%[&P&O-4c!m mEHQS3$QWokhhQ013P.7}|!CMN s1;ugD=#GBI^,cOWCoV'
 assert(decodeURIComponentCamel(encodeURIComponentCamel(rand)), rand)
 assert(decodeURIComponentCamel(encodeURIComponentCamel(sample)), sample)
-console.log(
-  sample.length,
-  encodeURIComponent(sample).length,
-  encodeURIComponentCamel(sample).length,
-  encodeURIComponentCamel(sample),
-)
+
+async function encodeDeflate(text) {
+  let stream = new Blob([text]).stream()
+
+  stream = stream.pipeThrough(new CompressionStream('deflate-raw'))
+
+  const res = await new Response(stream)
+  const blob = await res.blob()
+  const buffer = await blob.arrayBuffer()
+
+  return btoa(String.fromCharCode(...new Uint8Array(buffer)))
+}
+
+async function decodeDeflate(text) {
+  const binary = Uint8Array.from(atob(text), c => c.charCodeAt(0))
+
+  let stream = new Blob([binary]).stream()
+
+  stream = stream.pipeThrough(new DecompressionStream('deflate-raw'))
+
+  const res = await new Response(stream)
+  const blob = await res.blob()
+  return await blob.text()
+}
+
+async function testDeflate() {
+  const deflate = await encodeDeflate(sample)
+  const roundtripDeflate = await decodeDeflate(deflate)
+
+  assert(roundtripDeflate, sample)
+
+  console.log(
+    sample.length,
+    encodeURIComponent(sample).length,
+    encodeURIComponentCamel(sample).length,
+    deflate.length,
+    encodeURIComponentCamel(sample),
+  )
+}
+
+testDeflate()
